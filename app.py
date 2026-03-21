ce8ef90ec5a1482b75dc834ebe7a6d84e7061ef3
asfak440 <abdullahasfakfarvez@gmail.com>
2026-03-22 00:05:49 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,17 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 
-# ইভেন্ট লুপ ফিক্স
 nest_asyncio.apply()
 
 app = Flask(__name__)
 CORS(app)
 
-# ডাটাবেস কানেকশন
+# ডাটাবেস
 MONGO_URI = "mongodb+srv://abdullahasfakfarvezbd_db_user:Abdullah6790@cluster0.rmulyqq.mongodb.net/?appName=Cluster0"
 client_db = MongoClient(MONGO_URI)
 db = client_db['AAF_TeleEarn']
 users_col = db['users']
 
-# আপনার সবকটি HTML পেজ কানেক্ট করা হলো
 @app.route('/')
 @app.route('/dashboard')
 def dashboard(): return render_template('dashboard.html')
@@ -31,17 +29,12 @@ def task(): return render_template('task.html')
 @app.route('/trading')
 def trading(): return render_template('trading.html')
 
-@app.route('/wallet')
-def wallet(): return render_template('wallet.html')
-
 @app.route('/accounts')
 def accounts(): return render_template('accounts.html')
 
-# সার্ভার কানেকশন চেক করার এপিআই
-@app.route('/api/status', methods=['GET'])
-def status(): return jsonify({"status": "online"})
+@app.route('/wallet')
+def wallet(): return render_template('wallet.html')
 
-# পোর্ট ফিক্স
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
