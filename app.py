af8d5e115046f67d007232e42fe68212722b2b1c
asfak440 <abdullahasfakfarvez@gmail.com>
2026-04-15 16:29:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -324,4 +324,4 @@ def logout():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
