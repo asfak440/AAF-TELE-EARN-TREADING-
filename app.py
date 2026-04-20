eb7367309396e79a4c0140ae3771f9b91088e4ba
asfak440 <abdullahasfakfarvez@gmail.com>
2026-04-20 13:14:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app.config.update(
 
 # ================= APP =================
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY", "aaf_strong_secure_786)
+app.secret_key = os.getenv("SECRET_KEY", "aaf_strong_secure_786")
 
 CORS(app, supports_credentials=True)
 
