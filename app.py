8a3289455aa78ab9a30f3beaea7139ee03d6f8c2
asfak440 <abdullahasfakfarvez@gmail.com>
2026-04-21 18:12:54 +0600

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ CORS(app, supports_credentials=True)
 API_ID = int(os.environ.get("API_ID", 36466824))
 API_HASH = os.environ.get("API_HASH", "535ddcb85f2c3c74cc0ff532dd2c3406")
 FIREBASE_DB_URL = "https://teleearnbd-781d6-default-rtdb.firebaseio.com"
-FIREBASE_KEY_PATH = os.path.join(os.path.dirname(__file__), "firebase-key.json")
+FIREBASE_KEY_PATH = os.path.join(os.path.dirname(__file__), "firebase-adminsdk.json")
 MONGO_URI = os.environ.get("MONGO_URI")
 if not MONGO_URI:
     raise ValueError("MONGO_URI environment variable not set")
